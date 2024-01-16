cba4aae85fd9ff91b2e1bfe9a93710104aa3f0fb
Paul Norberger <panor@fastmail.com>
2024-01-16 16:42:48 +0100

    Add rendering of example texture to game loop

--- src/core/game_core.cpp
+++ src/core/game_core.cpp
@@ -7,11 +7,19 @@ int MainLoop() {
 
 	InitWindow(screenWidth, screenHeight, "Socket Test Game");
 
+	Texture2D player = LoadTexture("assets/Characters/green_character.png");
+	
+	Rectangle sourceRect{ 0.0f, 0.0f, player.width, player.height };
+	Rectangle destRect{ screenWidth / 2.0f, screenHeight / 2.0f, player.width, player.height };
+	Vector2 origin = { player.width / 2.0f, player.height / 2.0f };
+
 	while (!WindowShouldClose()) {
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
+		DrawTexturePro(player, sourceRect, destRect, origin, 0.0, RAYWHITE );
 		EndDrawing();
 	}
 
+	UnloadTexture(player);
 	return 0;
 }
