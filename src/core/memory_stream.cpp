8cac157b01e63589ae18adfaa514a65c3dd01449
Paul Norberger <panor@fastmail.com>
2024-01-18 13:17:33 +0100

    Switched out MemoryStream's for MemoryBitStream's

--- src/core/memory_stream.cpp
+++ src/core/memory_stream.cpp
@@ -1,39 +1,171 @@
 #include "memory_stream.hpp"
 
-void OutputMemoryStream::ReallocBuffer(uint32_t inNewLength)
+void OutputMemoryBitStream::WriteBits(uint8_t inData,
+	uint32_t inBitCount)
 {
-	buffer_ = static_cast<char*>(std::realloc(buffer_, inNewLength));
-	// TODO: Handle realloc failure
-	capacity_ = inNewLength;
-}
+	uint32_t nextBitHead = bit_head_ + static_cast<uint32_t>(inBitCount);
 
-void OutputMemoryStream::Write(const void* inData,
-	size_t inByteCount)
-{
-	//make sure we have space...
-	uint32_t resultHead = head_ + static_cast<uint32_t>(inByteCount);
-	if (resultHead > capacity_)
+	if (nextBitHead > bit_capacity_)
 	{
-		ReallocBuffer(std::max(capacity_ * 2, resultHead));
+		ReallocBuffer(std::max(bit_capacity_ * 2, nextBitHead));
 	}
 
-	//copy into buffer at head
-	std::memcpy(buffer_ + head_, inData, inByteCount);
+	//calculate the byteOffset into our buffer
+	//by dividing the head by 8
+	//and the bitOffset by taking the last 3 bits
+	uint32_t byteOffset = bit_head_ >> 3;
+	uint32_t bitOffset = bit_head_ & 0x7;
 
-	//increment head for next write
-	head_ = resultHead;
-}
+	uint8_t currentMask = ~(0xff << bitOffset);
+	buffer_[byteOffset] = (buffer_[byteOffset] & currentMask) | (inData << bitOffset);
 
-void InputMemoryStream::Read(void* outData,
-	uint32_t inByteCount)
-{
-	uint32_t resultHead = head_ + inByteCount;
-	if (resultHead > capacity_)
+	//calculate how many bits were not yet used in
+	//our target byte in the buffer
+	uint32_t bitsFreeThisByte = 8 - bitOffset;
+
+	//if we needed more than that, carry to the next byte
+	if (bitsFreeThisByte < inBitCount)
 	{
-		spdlog::error("InputMemoryStream::Read() - no data to read!");
+		//we need another byte
+		buffer_[byteOffset + 1] = inData >> bitsFreeThisByte;
 	}
 
-	std::memcpy(outData, buffer_ + head_, inByteCount);
+	bit_head_ = nextBitHead;
+}
 
-	head_ = resultHead;
+void OutputMemoryBitStream::WriteBits(const void* inData, uint32_t inBitCount)
+{
+	const char* srcByte = static_cast<const char*>(inData);
+	//write all the bytes
+	while (inBitCount > 8)
+	{
+		WriteBits(*srcByte, 8);
+		++srcByte;
+		inBitCount -= 8;
+	}
+	//write anything left
+	if (inBitCount > 0)
+	{
+		WriteBits(*srcByte, inBitCount);
+	}
+}
+
+void OutputMemoryBitStream::Write(const Vector3& in_vector)
+{
+	Write(in_vector.x);
+	Write(in_vector.y);
+	Write(in_vector.z);
+}
+
+void InputMemoryBitStream::Read(Vector3& out_vector)
+{
+	Read(out_vector.x);
+	Read(out_vector.y);
+	Read(out_vector.z);
+}
+
+void OutputMemoryBitStream::Write(const Quaternion& in_quat)
+{
+	float precision = (2.f / 65535.f);
+	Write(ConvertToFixed(in_quat.x, -1.f, precision), 16);
+	Write(ConvertToFixed(in_quat.y, -1.f, precision), 16);
+	Write(ConvertToFixed(in_quat.z, -1.f, precision), 16);
+	Write(in_quat.w < 0);
+}
+
+
+
+void OutputMemoryBitStream::ReallocBuffer(uint32_t inNewBitLength)
+{
+	if (buffer_ == nullptr)
+	{
+		//just need to memset on first allocation
+		buffer_ = static_cast<char*>(std::malloc(inNewBitLength >> 3));
+		memset(buffer_, 0, inNewBitLength >> 3);
+	}
+	else
+	{
+		//need to memset, then copy the buffer
+		char* tempBuffer = static_cast<char*>(std::malloc(inNewBitLength >> 3));
+		memset(tempBuffer, 0, inNewBitLength >> 3);
+		memcpy(tempBuffer, buffer_, bit_capacity_ >> 3);
+		std::free(buffer_);
+		buffer_ = tempBuffer;
+	}
+
+	//handle realloc failure
+	//...
+	bit_capacity_ = inNewBitLength;
+}
+
+
+void test1()
+{
+	OutputMemoryBitStream mbs;
+
+	mbs.WriteBits(11, 5);
+	mbs.WriteBits(52, 6);
+}
+
+void InputMemoryBitStream::ReadBits(uint8_t& out_data, uint32_t in_bit_count)
+{
+	uint32_t byte_offset = bit_head_ >> 3;
+	uint32_t bit_offset = bit_head_ & 0x7;
+
+	out_data = static_cast<uint8_t>(buffer_[byte_offset]) >> bit_offset;
+
+	uint32_t bits_free_this_byte = 8 - bit_offset;
+	if (bits_free_this_byte < in_bit_count)
+	{
+		//we need another byte
+		out_data |= static_cast<uint8_t>(buffer_[byte_offset + 1]) << bits_free_this_byte;
+	}
+
+	//don't forget a mask so that we only read the bit we wanted...
+	out_data &= (~(0x00ff << in_bit_count));
+
+	bit_head_ += in_bit_count;
+}
+
+void InputMemoryBitStream::ReadBits(void* out_data, uint32_t in_bit_count)
+{
+	uint8_t* dest_byte = reinterpret_cast<uint8_t*>(out_data);
+	//write all the bytes
+	while (in_bit_count > 8)
+	{
+		ReadBits(*dest_byte, 8);
+		++dest_byte;
+		in_bit_count -= 8;
+	}
+	//write anything left
+	if (in_bit_count > 0)
+	{
+		ReadBits(*dest_byte, in_bit_count);
+	}
+}
+
+void InputMemoryBitStream::Read(Quaternion& out_quat)
+{
+	float precision = (2.f / 65535.f);
+
+	uint32_t f = 0;
+
+	Read(f, 16);
+	out_quat.x = ConvertFromFixed(f, -1.f, precision);
+	Read(f, 16);
+	out_quat.y = ConvertFromFixed(f, -1.f, precision);
+	Read(f, 16);
+	out_quat.z = ConvertFromFixed(f, -1.f, precision);
+
+	out_quat.w = sqrtf(1.f -
+		out_quat.x * out_quat.y +
+		out_quat.y * out_quat.y +
+		out_quat.z * out_quat.z);
+	bool is_negative;
+	Read(is_negative);
+
+	if (is_negative)
+	{
+		out_quat.w *= -1;
+	}
 }
--- src/core/memory_stream.hpp
+++ src/core/memory_stream.hpp
@@ -1,51 +1,159 @@
-#pragma once
-
 #include <cstdint>
 #include <cstdlib>
-#include <memory>
-#include <spdlog/spdlog.h>
+#include <string>
 
-class OutputMemoryStream
+#include <raymath.h>
+
+class GameObject;
+
+inline uint32_t ConvertToFixed(float inNumber, float inMin, float inPrecision)
+{
+	return static_cast<int> ((inNumber - inMin) / inPrecision);
+}
+
+inline float ConvertFromFixed(uint32_t inNumber, float inMin, float inPrecision)
+{
+	return static_cast<float>(inNumber) * inPrecision + inMin;
+}
+
+
+class OutputMemoryBitStream
 {
 public:
-	OutputMemoryStream() : buffer_(nullptr), head_(0), capacity_(0)
+
+	OutputMemoryBitStream() :
+		bit_head_(0),
+		buffer_(nullptr)
 	{
-		ReallocBuffer(32);
+		ReallocBuffer(1500 * 8);
 	}
-	~OutputMemoryStream() { std::free(buffer_); }
 
-	//get a pointer to the data in the stream
+	~OutputMemoryBitStream() { std::free(buffer_); }
+
+	void WriteBits(uint8_t inData, uint32_t inBitCount);
+	void WriteBits(const void* inData, uint32_t inBitCount);
+
 	const char* GetBufferPtr() const { return buffer_; }
-	uint32_t GetLength() const { return head_; }
+	uint32_t GetBitLength() const { return bit_head_; }
+	uint32_t GetByteLength() const { return (bit_head_ + 7) >> 3; }
 
-	void Write(const void* inData, size_t inByteCount);
-	void Write(uint32_t inData) { Write(&inData, sizeof(inData)); }
-	void Write(int32_t inData) { Write(&inData, sizeof(inData)); }
+	void WriteBytes(const void* inData, uint32_t inByteCount) { WriteBits(inData, inByteCount << 3); }
+
+	/*
+	void Write( uint32_t inData, uint32_t inBitCount = 32 ) { WriteBits( &inData, inBitCount ); }
+	void Write( int inData, uint32_t inBitCount = 32 ) { WriteBits( &inData, inBitCount ); }
+	void Write( float inData ) { WriteBits( &inData, 32 ); }
+
+	void Write( uint16_t inData, uint32_t inBitCount = 16 ) { WriteBits( &inData, inBitCount ); }
+	void Write( int16_t inData, uint32_t inBitCount = 16 ) { WriteBits( &inData, inBitCount ); }
+
+	void Write( uint8_t inData, uint32_t inBitCount = 8 ) { WriteBits( &inData, inBitCount ); }
+	*/
+
+	template< typename T >
+	void Write(T inData, uint32_t inBitCount = sizeof(T) * 8)
+	{
+		static_assert(std::is_arithmetic< T >::value ||
+			std::is_enum< T >::value,
+			"Generic Write only supports primitive data types");
+		WriteBits(&inData, inBitCount);
+	}
+
+	void Write(bool inData) { WriteBits(&inData, 1); }
+
+	void Write(const Vector3& in_vector);
+	void Write(const Quaternion& in_quat);
+
+	void Write(const std::string& inString)
+	{
+		uint32_t elementCount = static_cast<uint32_t>(inString.size());
+		Write(elementCount);
+		for (const auto& element : inString)
+		{
+			Write(element);
+		}
+	}
 
 private:
-	void ReallocBuffer(uint32_t inNewLength);
+	void		ReallocBuffer(uint32_t inNewBitCapacity);
 
 	char* buffer_;
-	uint32_t head_;
-	uint32_t capacity_;
+	uint32_t bit_head_;
+	uint32_t bit_capacity_;
 };
 
-class InputMemoryStream
+class InputMemoryBitStream
 {
 public:
-	InputMemoryStream(char* inBuffer, uint32_t inByteCount) :
-		buffer_(inBuffer), capacity_(inByteCount), head_(0) {}
 
-	~InputMemoryStream() { std::free(buffer_); }
+	InputMemoryBitStream(char * in_buffer, uint32_t in_bit_count) :
+		buffer_(in_buffer),
+		bit_capacity_(in_bit_count),
+		bit_head_(0),
+		is_buffer_owner_(false) {}
 
-	uint32_t GetRemainingDataSize() const { return capacity_ - head_; }
+	InputMemoryBitStream(const InputMemoryBitStream& in_other) :
+		bit_capacity_(in_other.bit_capacity_),
+		bit_head_(in_other.bit_head_),
+		is_buffer_owner_(true)
+	{
+		//allocate buffer of right size
+		int byte_count = bit_capacity_ / 8;
+		buffer_ = static_cast<char*>(malloc(byte_count));
+		//copy
+		memcpy(buffer_, in_other.buffer_, byte_count);
+	}
 
-	void Read(void* outData, uint32_t inByteCount);
-	void Read(uint32_t& outData) { Read(&outData, sizeof(outData)); }
-	void Read(int32_t& outData) { Read(&outData, sizeof(outData)); }
+	~InputMemoryBitStream() { if (is_buffer_owner_) { free(buffer_); }; }
+
+	const char* GetBufferPtr() const { return buffer_; }
+	uint32_t GetRemainingBitCount() const { return bit_capacity_ - bit_head_; }
+
+	void ReadBits(uint8_t& out_data, uint32_t in_bit_count);
+	void ReadBits(void* out_data, uint32_t in_bit_count);
+
+	void ReadBytes(void* out_data, uint32_t in_byte_count) { ReadBits(out_data, in_byte_count << 3); }
+
+	template< typename T >
+	void Read(T& in_data, uint32_t in_bit_count = sizeof(T) * 8)
+	{
+		static_assert(std::is_arithmetic< T >::value ||
+			std::is_enum< T >::value,
+			"Generic Read only supports primitive data types");
+		ReadBits(&in_data, in_bit_count);
+	}
+
+	void Read(uint32_t& out_data, uint32_t in_bit_count = 32) { ReadBits(&out_data, in_bit_count); }
+	void Read(int& out_data, uint32_t in_bit_count = 32) { ReadBits(&out_data, in_bit_count); }
+	void Read(float& out_data) { ReadBits(&out_data, 32); }
+
+	void Read(uint16_t& out_data, uint32_t in_bit_count = 16) { ReadBits(&out_data, in_bit_count); }
+	void Read(int16_t& out_data, uint32_t in_bit_count = 16) { ReadBits(&out_data, in_bit_count); }
+
+	void Read(uint8_t& out_data, uint32_t in_bit_count = 8) { ReadBits(&out_data, in_bit_count); }
+	void Read(bool& out_data) { ReadBits(&out_data, 1); }
+
+	void Read(Quaternion& out_quat);
+
+	void ResetToCapacity(uint32_t in_byte_capacity) { bit_capacity_ = in_byte_capacity << 3; bit_head_ = 0; }
+
+
+	void Read(std::string& in_string)
+	{
+		uint32_t element_count;
+		Read(element_count);
+		in_string.resize(element_count);
+		for (auto& element : in_string)
+		{
+			Read(element);
+		}
+	}
+
+	void Read(Vector3 & out_vector);
 
 private:
 	char* buffer_;
-	uint32_t head_;
-	uint32_t capacity_;
+	uint32_t bit_head_;
+	uint32_t bit_capacity_;
+	bool is_buffer_owner_;
 };
